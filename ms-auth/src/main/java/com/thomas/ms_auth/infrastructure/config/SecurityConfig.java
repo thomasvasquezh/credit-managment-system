b1001f8a3fb142e8d749ab594525a1ab3fbe81b3
thomasvasquezh <tom080901@hotmail.com>
2026-05-04 13:35:55 -0500

    configure initial auth service setup

---
+++ ms-auth/src/main/java/com/thomas/ms_auth/infrastructure/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.thomas.ms_auth.infrastructure.config;
+
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/v3/api-docs/**",
+                                "/openapi/**",
+                                "/actuator/health"
+                        ).permitAll()
+                        .requestMatchers(HttpMethod.GET, "/openapi/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form.disable())
+                .httpBasic(httpBasic -> httpBasic.disable())
+                .build();
+    }
+}
